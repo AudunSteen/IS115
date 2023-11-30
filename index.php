592d63400619f2b8afcca4078aa403031a9b7f34
Audun Steen <89306928+AudunSteen@users.noreply.github.com>
2023-11-30 23:11:20 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -11,9 +11,14 @@
         <li><a href="modul3/index.php">Modul 3</a></li>
         <li><a href="modul4/index.php">Modul 4</a></li>
         <li><a href="modul5/index.php">Modul 5</a></li>
+        <li><a href="modul06/index.php">Modul 6</a></li>
+        <li><a href="modul7/index.php">Modul 7</a></li>
+        <li><a href="modul9/index.php">Modul 9</a></li>
+        <li><a href="modul10/index.php">Modul 10</a></li>
+
+        <li><a href="bok_ovinger_is115">bok  forslag</a></li>
 
         <li><a href="timeoppgaver/index.php">notater fra timen</a></li>
-
     </ul>
 
  
